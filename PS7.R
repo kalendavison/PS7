a3b9cd200e7f7d5d0a8187da587ac565ceb020fc
Kalen Davison <davison@wustl.edu>
2018-04-23 15:24:06 -0500

    Completed problems 2-3

--- PS7.R
+++ PS7.R
@@ -8,33 +8,43 @@ setwd("/Users/kalendavison/Desktop/Applied Statistical Programming/GitHub/PS7")
 crime_data = read.csv("March2018.csv")
 
 #2)
-substr(c)
+install.packages("lubridate")
+library(lubridate)
+
+crime_data$date = strptime(crime_data$DateOccur, "%m/%d/%Y %H:%M")
+crime_data$date = as.Date(crime_data$date, "%m/%d/%Y")
+crime_data$date = as.character(crime_data$date)
+
 crime_data = crime_data %>%
-  group_by(Description)  %>%
-  summarise(total=n())
-crime_data = mutate(crime_data, crimes_per_day = (total/31))
+  group_by(Description, date)  %>%
+  summarise(count=n())
+
+which.max(crime_data$count)
+crime_data$count[1125]
+crime_data$Description[1125]
+
+### Leaving the scene of an accident was the most common crime in March, occurring 24 times.
+
 
 
 #3)
-neighborhood_crime <- crime_data %>% 
-  group_by(Neighborhood) %>% 
-  summarise(total=n())
-neighborhood_crime<-mutate(neighborhood_crime, crimes_per_day=(total/31))
-neighborhood_crime<-select(neighborhood_crime, Neighborhood, crimes_per_day)
+crime_data_neighborhood = read.csv("March2018.csv")
+library(lubridate)
+crime_data_neighborhood$date = strptime(crime_data_neighborhood$DateOccur, "%m/%d/%Y %H:%M")
+crime_data_neighborhood$date = as.Date(crime_data_neighborhood$date, "%m/%d/%Y")
+crime_data_neighborhood$date = as.character(crime_data_neighborhood$date)
+crime_data_neighborhood = crime_data_neighborhood %>%
+  group_by(Description, Neighborhood)  %>%
+  summarise(count=n())
 
-neighborhood_crime %>%
-  summarise(max(crimes_per_day))
-  
+which.max(crime_data_neighborhood$count)
+crime_data_crime_data_neighborhood$count[1335] 
+#Neighborhood 47 has the most crime with 1335 crimes in March.
 
-  
+#4)
 
-#For this problem set, you will need to use dplyr and ggplot2 R packages to summarize a given dataset and create an appropriate visualization. You will
-#have to only use the functions within dplyr (ex. filter(),select(),summarise(), and mutate()) and pipe operator (%>%) to work with data. Please complete
-#the following tasks in order:
- # 1. Go to this link and download March2018 crime dataset: http://www. slmpd.org/Crimereports.shtml
-#2. Compute the number of crimes per day by the type of crime (Hint: clean Description variable and use it). 
-#Which types of crime happened the most in March?
-#3. Compute the number of crime per day by neighborhood. Which neigh- borhood has the most number of crime?
+
+
 
 #4. Compute the proportion of crime related to robbery by district. Which district has the largest proportion of crime related to robbery?
 #5. Visualize changes of all types of crime over time using ggplot2. Write appropriate labels and titles.
