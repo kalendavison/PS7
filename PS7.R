05f858aae95c6116998ef22431b632d3c3384adb
Kalen Davison <davison@wustl.edu>
2018-04-25 21:45:03 -0500

    Finished problem 4

--- PS7.R
+++ PS7.R
@@ -25,8 +25,6 @@ crime_data$Description[1125]
 
 ### Leaving the scene of an accident was the most common crime in March, occurring 24 times.
 
-
-
 #3)
 crime_data_neighborhood = read.csv("March2018.csv")
 library(lubridate)
@@ -38,14 +36,34 @@ crime_data_neighborhood = crime_data_neighborhood %>%
   summarise(count=n())
 
 which.max(crime_data_neighborhood$count)
-crime_data_crime_data_neighborhood$count[1335] 
+crime_data_neighborhood$count[1335] 
 #Neighborhood 47 has the most crime with 1335 crimes in March.
+
 
 #4)
 
+crime_data_robbery = read.csv("March2018.csv")
+crime_data_robbery = crime_data_robbery %>%
+mutate(robbery = as.numeric(grepl("ROBBERY", Description, ignore.case=T)),
+       District = District)
+
+crime_data_robbery = crime_data_robbery %>%
+  group_by(District) %>%
+  filter(robbery == 1) %>%
+  summarise(count=n())
+View(crime_data_robbery)        
+
+crime_data_district = read.csv("March2018.csv")
+crime_data_district = crime_data_district %>% 
+  group_by(District) %>%
+  summarise(count=n())
+crime_data_district = crime_data_district[2:7,]
+View(crime_data_district)
+proportion_robbery = crime_data_robbery$count/crime_data_district$count
+which.max(proportion_robbery) #Returns indexed 5, equivalent to district 5
+#District 5 has the largest proportion of crime related to robbery.
 
 
 
-#4. Compute the proportion of crime related to robbery by district. Which district has the largest proportion of crime related to robbery?
 #5. Visualize changes of all types of crime over time using ggplot2. Write appropriate labels and titles.
 #6. Visualize changes of all types of crime over time by district using ggplot2. Choose different color to indicate each district. Write ap- propriate legend, labels and titles.
